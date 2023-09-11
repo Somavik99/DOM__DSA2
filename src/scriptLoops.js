5048a000ca0a5c6bbb721888d7b9f1665da16e68
Avik Roy <somavik99@gmail.com>
2023-09-12 01:10:58 +0530

    main

--- src/scriptLoops.js
+++ src/scriptLoops.js
@@ -25,23 +25,54 @@ function ReverseNum(num) {
 
 console.log(ReverseNum(1234));
 
-
-function Occurrence(str){
-const occ = str.split("").reduce((acc,curr)=>{
-if(acc.hasOwnProperty(curr)){
-    acc[curr]= ++acc[curr];
-}else{
-    acc[curr]=1
-}
-return acc
-},{})
-return occ;
-}
-
-console.log(Occurrence("Banannna"));
-
-function Retrieve(arr, n=1){
-
+function Occurrence(str) {
+  const occ = str.split("").reduce((acc, curr) => {
+    if (acc.hasOwnProperty(curr)) {
+      acc[curr] = ++acc[curr];
+    } else {
+      acc[curr] = 1;
+    }
+    return acc;
+  }, {});
+  return occ;
 }
 
-Retrieve([1,2,3,4,5])
+console.log(Occurrence("Hahhhhahahhahha"));
+
+function Retrieve(arr, n = 1) {
+  if (n <= arr.length) {
+    for (let i = n; i > 0; i--) {
+      console.log(arr[i]);
+    }
+  } else {
+    console.log(n + `  elements is not present `);
+  }
+}
+
+Retrieve([1, 2, 3, 4, 5], 4);
+
+function Frequent(arr) {
+  const NewArr = arr.reduce((acc, curr) => {
+    if (acc.hasOwnProperty(curr)) {
+      acc[curr] = ++acc[curr];
+    } else {
+      acc[curr] = 1;
+    }
+    return acc;
+  }, {});
+  
+  return NewArr;
+}
+
+console.log(Frequent([1, 2, 5, 4, 8, 9, 7, 5, 6, 4, 1, 2, 3, 5]));
+
+
+let MainDiv =  document.getElementById('main');
+console.log(MainDiv)
+
+let NewDiv= document.createElement('div');
+MainDiv.appendChild(NewDiv);
+console.log(NewDiv)
+
+let Input = document.getElementsByName('input');
+console.log(Input)
