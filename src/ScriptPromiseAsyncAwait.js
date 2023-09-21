8ae1dd2c458bfef38e65c0f5744bdb34d63e9117
Avik Roy <somavik99@gmail.com>
2023-09-21 23:25:18 +0530

    main

--- src/ScriptPromiseAsyncAwait.js
+++ src/ScriptPromiseAsyncAwait.js
@@ -1,12 +1,49 @@
 console.log("/////AsyncAwaitPromises/////");
 
 function MakeRequest(Location) {
+  console.log("////MakeRequest////");
+  const TimerID = setTimeout(() => {
+    console.log("////MakeRequestTimerID////");
+    const CheckingReturn = function (arr = [], callBack) {
+      console.log("////ArrayInput Function////");
+      let arr2 = [1, 2, 32, 4, 5, 6, 78, 66];
+      let ArrCheck = function () {
+      
+        for (let i = 0; i <= arr2.length - 1; i++) {
+          if (arr.length !== arr2.length) {
+            setTimeout(() => {
+              console.log("////LoopTimer////");
 
-  console.log(
-    setTimeout(() => {
-      return console.log("AsyncLog");
-    }, 6000),
-    console.log("AnotherLog: " + " " + Location)
-  );
+              arr.push(arr2[i]);
+              let NewArr = arr;
+              console.log(NewArr.length);
+              //   console.log(Object.keys(arr));
+            }, 1000);
+
+            // console.log(LoopTimer);
+          } else {
+            console.log(arr);
+          }
+        }
+     
+      };
+      return ArrCheck();
+    };
+
+    const CallBackFunc = setTimeout(() => {
+      return console.log("API Calling");
+    }, 3000);
+    console.log(Location);
+
+    CheckingReturn([], CallBackFunc);
+    console.log(TimeRanges);
+
+    // return () => {
+    //   clearTimeout();
+    // };
+  }, 2000);
+  return () => {
+    clearTimeout(TimerID, CallBackFunc);
+  };
 }
 MakeRequest("Delhi");
